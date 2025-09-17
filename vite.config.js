7ad3fae00a2627a80fe7b5dbcdb99454680b4f84
SamSoghomonyan <sam.soghomonyan@bk.ru>
2025-09-17 13:37:04 +0400

    game

--- vite.config.js
+++ vite.config.js
@@ -5,30 +5,30 @@ import checker from "vite-plugin-checker";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  base: "./",
-  build: {
-    sourcemap: true,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-      },
+    base: "/game/", // <-- Change this from "./" to "/game/"
+    build: {
+        sourcemap: true,
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+            },
+        },
+        cssCodeSplit: false,
     },
-    cssCodeSplit: false,
-  },
-  css: {
-    postcss: {
-      plugins: [autoprefixer()],
+    css: {
+        postcss: {
+            plugins: [autoprefixer()],
+        },
     },
-  },
-  plugins: [
-    tsconfigPaths(),
-    checker({
-      typescript: true,
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [
+        tsconfigPaths(),
+        checker({
+            typescript: true,
+        }),
+    ],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
 });
